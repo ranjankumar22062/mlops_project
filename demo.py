86bf0539dc49c19073036b17787c616c25e71187
Ranjan Kumar <meb22062@gamil.com>
2025-09-14 13:50:18 +0530

    logging done and exception done

---
+++ demo.py
@@ -0,0 +1,14 @@
+from src.logger import logging
+
+logging.debug("This is a debug message")
+
+
+from src.exception import MyException
+import sys
+
+
+try:
+    a=1+z
+except Exception as e:
+    logging.info("We are logging the error now")
+    raise MyException(e, sys) from e    
