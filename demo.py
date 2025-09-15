41f8ba591b2fcd50c533958032ecdee1233328a3
Ranjan Kumar <meb22062@gamil.com>
2025-09-15 19:42:18 +0530

    data_ingestion doe from mongodb database

--- demo.py
+++ demo.py
@@ -1,14 +1,17 @@
-from src.logger import logging
+# from src.logger import logging
 
-logging.debug("This is a debug message")
-
-
-from src.exception import MyException
-import sys
+# logging.debug("This is a debug message")
 
 
-try:
-    a=1+z
-except Exception as e:
-    logging.info("We are logging the error now")
-    raise MyException(e, sys) from e    
+# from src.exception import MyException
+# import sys
+
+
+# try:
+#     a=1+z
+# except Exception as e:
+#     logging.info("We are logging the error now")
+#     raise MyException(e, sys) from e    
+from src.pipline.training_pipeline import TrainPipeline 
+pipeline=TrainPipeline()
+pipeline.run_pipeline()
