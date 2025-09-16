c9c025eee070a56c88be5e230223b0a8f0a456c3
Ranjan Kumar <meb22062@gamil.com>
2025-09-16 13:48:06 +0530

    schema.yaml fiel change

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -10,3 +10,10 @@ class DataValidationArtifact:
     validation_status:bool
     validation_report_file_path:str
     message:str
+
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path:str 
+    transformed_train_file_path:str
+    transformed_test_file_path:str
+
--- src/pipline/training_pipeline.py
+++ src/pipline/training_pipeline.py
@@ -3,21 +3,23 @@ from src.exception import MyException
 from src.logger import logging
 
 from src.components.data_ingestion import DataIngestion
-from src.components.data_validation import DataValidation   
+from src.components.data_validation import DataValidation
+from src.components.data_transformation import DataTransformation
+
 
 from src.entity.config_entity import (DataIngestionConfig,
                                       
                                       
-                                      DataValidationConfig)
-                                        #   DataTransformationConfig,
+                                      DataValidationConfig,
+                                      DataTransformationConfig)
                                         #   ModelTrainerConfig,
                                         #   ModelEvaluationConfig,
                                         #   ModelPusherConfig)
                                           
 from src.entity.artifact_entity import (DataIngestionArtifact,
                                         
-                                        DataValidationArtifact)
-                                            # DataTransformationArtifact,
+                                        DataValidationArtifact,
+                                        DataTransformationArtifact)
                                             # ModelTrainerArtifact,
                                             # ModelEvaluationArtifact,
                                             # ModelPusherArtifact)
@@ -28,7 +30,7 @@ class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
-        # self.data_transformation_config = DataTransformationConfig()
+        self.data_transformation_config = DataTransformationConfig()
         # self.model_trainer_config = ModelTrainerConfig()
         # self.model_evaluation_config = ModelEvaluationConfig()
         # self.model_pusher_config = ModelPusherConfig()
@@ -68,10 +70,25 @@ class TrainPipeline:
             return data_validation_artifact
         except Exception as e:
             raise MyException(e, sys) from e
+    def start_data_transformation(self, data_ingestion_artifact: DataIngestionArtifact, data_validation_artifact: DataValidationArtifact) -> DataTransformationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data transformation component
+        """
+        try:
+            data_transformation = DataTransformation(data_ingestion_artifact=data_ingestion_artifact,
+                                                     data_transformation_config=self.data_transformation_config,
+                                                     data_validation_artifact=data_validation_artifact)
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
+        except Exception as e:
+            raise MyException(e, sys)        
             
     def run_pipeline(self, )   -> None: 
         try:
             data_ingestion_artifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_transformation_artifact=self.start_data_transformation(
+                data_ingestion_artifact=data_ingestion_artifact,data_validation_artifact=data_validation_artifact
+            )
         except Exception as e:
             raise MyException(e, sys) from e   
